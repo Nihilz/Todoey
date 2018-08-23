c93361adf85444241f1f312dc7fdfc53b0dcce58
Andrew Pepper <andrewpepper@andrews-mini>
2018-08-23 02:50:54 +0100

    Added functionality for Category Cells to be deleted by swiping far enough to the right.

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -124,8 +124,6 @@ extension CategoryViewController: SwipeTableViewCellDelegate {
                 } catch {
                     print("Error deleting category, \(error)")
                 }
-                
-                tableView.reloadData()
             }
         }
         
@@ -133,6 +131,12 @@ extension CategoryViewController: SwipeTableViewCellDelegate {
         deleteAction.image = UIImage(named: "delete-icon")
         
         return [deleteAction]
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
+        var options = SwipeOptions()
+        options.expansionStyle = .destructive
+        return options
     }
     
 }
