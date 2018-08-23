fbc55283eefabd736b29a354a37261924e56ba73
Andrew Pepper <andrewpepper@andrews-mini>
2018-08-23 18:29:17 +0100

    Multiple TodoListViewController cells display as a colour gradient. The colour of the gradient is determined by the colour of the item's related category. Text colour in each item cell is dynamic and is determined by the darkness of the cell background colour. .

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import ChameleonFramework
 
 class TodoListViewController: SwipeTableViewController {
 
@@ -23,6 +24,8 @@ class TodoListViewController: SwipeTableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.separatorStyle = .none
+        
         //print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
     }
@@ -37,10 +40,14 @@ class TodoListViewController: SwipeTableViewController {
             
             cell.textLabel?.text = item.title
             
-            cell.accessoryType = item.done ? .checkmark : .none
-        } else {
-            cell.textLabel?.text = "No Items Added Yet"
-        }
+            if let colour = UIColor(hexString: selectedCategory!.colour)?.darken(byPercentage: CGFloat(indexPath.row) / CGFloat(todoItems!.count)) {
+                cell.backgroundColor = colour
+                cell.textLabel?.textColor = ContrastColorOf(colour, returnFlat: true)
+            }
+                cell.accessoryType = item.done ? .checkmark : .none
+            } else {
+                cell.textLabel?.text = "No Items Added Yet"
+            }
         
         return cell
     }
